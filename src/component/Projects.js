e26c3193e511245930577469849b4c47f2a9caff
craigweighman <45236597+craigweighman@users.noreply.github.com>
2020-04-28 16:26:11 +0100

    Update Projects.js

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -17,10 +17,17 @@ class Projects extends Component {
               <br />
             </div>
           }
-          title=""
-          text=""
-          link=""
-          target=""
+    
+      title="Good design is innovative"
+          text="Good design makes a product useful Good
+              design is aesthetic Good design makes a product understandable
+              Good design is unobtrusive Good design is honest Good design is
+              long lasting Good design is thorough down to the last detail Good
+              design is environmentally friendly Good design is as little design
+          as possible | more..."
+          link="https://the-wisdom-of-rams.netlify.com/"
+          target="_blank"
+      
         />
         <Example
           indent={
