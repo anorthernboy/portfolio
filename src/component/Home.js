754b90c87be2ff6b0c16a9ff4ee0f32f6dd78576
craigweighman <45236597+craigweighman@users.noreply.github.com>
2020-04-28 18:56:15 +0100

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -7,8 +7,7 @@ class Home extends Component {
     return (
       <div className="home-container" id="home">
         <Example
-          title="There is no learning without trying"
-          text="lots of careers. So said Jony Ive (well, almost). Product Manager and expert-generalist with 5+ years of product development experience plus time spent working as a Software Developer. Business mentor to 30+ start-up entrepreneurs. Would-be creative with left-brain sensibilities, living a life of essentialism and trying to do the most good I can do. Reach out to discuss social-impact tech, the North, and the product output of Braun between 1961 and 1995."
+          title="There is no learning without trying lots of careers. So said Jony Ive (well, almost). Product Manager and expert-generalist with 5+ years of product development experience plus time spent working as a Software Developer. Business mentor to 30+ start-up entrepreneurs. Would-be creative with left-brain sensibilities, living a life of essentialism and trying to do the most good I can do. Reach out to discuss social-impact tech, the North, and the product output of Braun between 1961 and 1995."
         />
       </div>
     );
